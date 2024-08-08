b699723e315ab56fb6d05b4e095dafedc78710fa
Mikhail Shutov <shutovmik8@gmail.com>
2024-08-08 19:46:13 +0300

    Message

--- simple-vector/array_ptr.h
+++ simple-vector/array_ptr.h
@@ -1,3 +1,57 @@
-// вставьте сюда ваш код для класса ArrayPtr
-// внесите в него изменения, 
-// которые позволят реализовать move-семантику
+template <typename Type>
+class ArrayPtr {
+public:
+    ArrayPtr() = default;
+
+    explicit ArrayPtr(size_t size) {
+        if (size) {
+            raw_ptr_ = new Type[size];
+        }
+    }
+
+    explicit ArrayPtr(Type* raw_ptr) noexcept {
+        raw_ptr_ = raw_ptr;
+    }
+
+    ArrayPtr(const ArrayPtr&) = delete;
+
+    ~ArrayPtr() {
+        delete [] raw_ptr_;
+    }
+
+    ArrayPtr& operator=(const ArrayPtr&) = delete;
+
+    [[nodiscard]] Type* Release() noexcept {
+        Type* copy = raw_ptr_;
+        raw_ptr_ = nullptr;
+        return copy;
+    }
+
+    Type& operator[](size_t index) noexcept {
+        return raw_ptr_[index];
+    }
+
+    const Type& operator[](size_t index) const noexcept {
+        return raw_ptr_[index];
+    }
+
+    explicit operator bool() const {
+        if (raw_ptr_) {
+            return true;
+        }
+        return false;
+    }
+
+    Type* Get() const noexcept {
+        return raw_ptr_;
+    }
+
+    void swap(ArrayPtr& other) noexcept {
+        Type* tmp = raw_ptr_;
+        raw_ptr_ = other.raw_ptr_;
+        other.raw_ptr_ = tmp;
+    }
+
+private:
+    Type* raw_ptr_ = nullptr;
+};
--- simple-vector/simple_vector.h
+++ simple-vector/simple_vector.h
@@ -1,2 +1,301 @@
-// вставьте сюда ваш код для класса SimpleVector
-// внесите необходимые изменения для поддержки move-семантики
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <initializer_list>
+#include <iostream>
+#include <iterator>
+#include <stdexcept>
+#include <utility>
+
+#include "array_ptr.h"
+
+class ReserveProxyObj {
+public:
+    ReserveProxyObj(size_t capacity) : capacity_(capacity) {}
+
+    size_t capacity_{};
+};
+
+ReserveProxyObj Reserve(size_t capacity_to_reserve) {
+    return ReserveProxyObj(capacity_to_reserve);
+};
+
+template <typename Type>
+class SimpleVector {
+public:
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
+
+    SimpleVector() noexcept = default;
+
+    explicit SimpleVector(size_t size) : size_{size}, capacity_{size} {
+        ArrayPtr<Type> tmp{size};
+        items_.swap(tmp);
+        std::fill(begin(), end(), Type());
+    }
+
+    SimpleVector(size_t size, const Type& value) : size_{size}, capacity_{size} {
+        ArrayPtr<Type> tmp{size};
+        items_.swap(tmp);
+        std::fill(begin(), end(), value);
+    }
+
+    SimpleVector(std::initializer_list<Type> init) : size_{init.size()}, capacity_{init.size()}  {
+        make_new_copy_vector(init.size(), init.begin(), init.end());        
+    }
+
+    SimpleVector(const SimpleVector& other) : size_{other.size_}, capacity_{other.size_}  {
+        make_new_copy_vector(other.size_, other.begin(), other.end());
+    }
+
+    SimpleVector(SimpleVector&& other) {
+        make_new_move_vector(other.size_, other.begin(), other.end());
+        size_ = std::exchange(other.size_, 0);
+        capacity_ = std::exchange(other.capacity_, 0);
+    }
+
+    SimpleVector(ReserveProxyObj a) {
+        ArrayPtr<Type> tmp(a.capacity_);
+        items_.swap(tmp);
+        capacity_ = a.capacity_;
+    }
+
+    SimpleVector& operator=(const SimpleVector& rhs) {
+        if (&rhs == this) {
+            return *this;
+        }
+        size_t size{rhs.size_};
+        make_new_copy_vector(size, rhs.begin(), rhs.end());
+        size_ = size;
+        capacity_ = size;
+        return *this;
+    }
+
+    SimpleVector& operator=(SimpleVector&& rhs) {
+        if (&rhs == this) return *this;
+        size_t size{rhs.size_};
+        make_new_move_vector(size, rhs.begin(), rhs.end());   
+        static_cast<void>(rhs.items_.Release());              //Чтобы в rhs.items_ записать nullptr, вызовем Release()
+        size_ = std::exchange(rhs.size_, 0);                  //Чтобы обойти warning из-за [[nodiscard]] в Release(), приведем к void
+        capacity_ = std::exchange(rhs.capacity_, 0);
+        return *this;
+    }
+
+    void PushBack(const Type& item) {
+        if (size_ == capacity_) {
+            size_t new_capacity{(capacity_) ? (capacity_ * 2) : 1};
+            make_new_move_vector(new_capacity, begin(), end());
+            capacity_ = new_capacity;
+        }
+        items_[size_] = item;
+        ++size_;
+    }
+
+    void PushBack(Type&& item) {
+        if (size_ == capacity_) {
+            size_t new_capacity{(capacity_) ? (capacity_ * 2) : 1};
+            make_new_move_vector(new_capacity, begin(), end());
+            capacity_ = new_capacity;
+        }
+        items_[size_] = std::move(item);
+        ++size_;
+    }
+
+    Iterator Insert(ConstIterator pos, const Type& value) {
+        int index{static_cast<int>(std::distance(cbegin(), pos))};
+        if (size_ == capacity_) {
+            size_t new_capacity{capacity_ ? (capacity_ * 2) : 1};
+            make_new_move_vector(new_capacity, begin(), end());
+            capacity_ = new_capacity;
+        }
+        items_[index] = value;
+        ++size_;
+        return begin() + index;
+    }
+
+    Iterator Insert(ConstIterator pos, Type&& value) {
+        int index{static_cast<int>(std::distance(cbegin(), pos))};
+        if (size_ == capacity_) {
+            size_t new_capacity{capacity_ ? (capacity_ * 2) : 1};
+            make_new_move_vector(new_capacity, begin(), end());
+            capacity_ = new_capacity;
+        }
+        items_[index] = std::move(value);
+        ++size_;
+        return begin() + index;
+    }
+
+    void PopBack() noexcept {
+        if (size_ == 0) {
+            return;
+        }
+        --size_;
+    }
+
+    Iterator Erase(ConstIterator pos) {
+        if (size_ == 0) {
+            return nullptr;
+        }
+        int index{static_cast<int>(std::distance(cbegin(), pos))};
+        std::move(begin() + index + 1, end(), begin() + index);
+        --size_;
+        return begin() + index;
+    }
+
+    void swap(SimpleVector& other) noexcept {
+        items_.swap(other.items_);
+        std::swap(size_, other.size_);
+        std::swap(capacity_, other.capacity_);
+    }
+
+    void Reserve(size_t new_capacity) {
+        if (new_capacity > capacity_) {
+            make_new_move_vector(new_capacity, begin(), end());
+            capacity_ = new_capacity;
+        }
+    }
+
+    size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    size_t GetCapacity() const noexcept {
+        return capacity_;
+    }
+
+    bool IsEmpty() const noexcept {
+        if (size_) {
+            return false;
+        }
+        return true;
+    }
+
+    Type& operator[](size_t index) noexcept {
+        return items_[index];
+    }
+
+    const Type& operator[](size_t index) const noexcept {
+        return items_[index];
+    }
+
+    Type& At(size_t index) {
+        if (index >= size_) {
+            throw std::out_of_range("bad");
+        }
+        return items_[index];
+    }
+
+    const Type& At(size_t index) const {
+        if (index >= size_) {
+            throw std::out_of_range("bad");
+        }
+        return items_[index];
+    }
+
+    void Clear() noexcept {
+        size_ = 0;
+    }
+
+    void Resize(size_t new_size) {
+        if (new_size <= size_) {
+            size_ = new_size;
+            return;
+        }
+        if (new_size <= capacity_) {
+            auto it = end();
+            while (it != begin() + new_size) {
+                *it = Type();
+                ++it;
+            }
+            size_ = new_size;
+            return;
+        }
+        size_t new_capacity{std::max(new_size, capacity_ * 2)};
+        make_new_move_vector(new_capacity, begin(), end());
+        auto it = end();
+        while (it != begin() + new_size) {
+            *it = Type(); 
+            ++it;
+        }
+        size_ = new_size;
+        capacity_ = new_capacity;
+    }
+
+    Iterator begin() noexcept {
+        return items_.Get();
+    }
+
+    Iterator end() noexcept {
+        return items_.Get() + size_;
+    }
+
+    ConstIterator begin() const noexcept {
+        return items_.Get();
+    }
+
+    ConstIterator end() const noexcept {
+        return items_.Get() + size_;
+    }
+
+    ConstIterator cbegin() const noexcept {
+        return items_.Get();
+    }
+
+    ConstIterator cend() const noexcept {
+        return items_.Get() + size_;
+    }
+
+
+
+private:
+    ArrayPtr<Type> items_;
+
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+    template <typename It>
+    void make_new_copy_vector(size_t new_value, It begin, It end) {
+        ArrayPtr<Type> tmp(new_value);
+        std::copy(begin, end, tmp.Get());
+        items_.swap(tmp);
+    }
+
+    template <typename It>
+    void make_new_move_vector(size_t new_value, It begin, It end) {
+        ArrayPtr<Type> tmp(new_value);
+        std::move(begin, end, tmp.Get());
+        items_.swap(tmp);
+    }
+};
+
+template <typename Type>
+inline bool operator==(const SimpleVector<Type>& lhs, const SimpleVector<Type>& rhs) {
+    return std::equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
+}
+
+template <typename Type>
+inline bool operator!=(const SimpleVector<Type>& lhs, const SimpleVector<Type>& rhs) {
+    return !(lhs == rhs);
+}
+
+template <typename Type>
+inline bool operator<(const SimpleVector<Type>& lhs, const SimpleVector<Type>& rhs) {
+    return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+template <typename Type>
+inline bool operator<=(const SimpleVector<Type>& lhs, const SimpleVector<Type>& rhs) {
+    return !(rhs < lhs);
+}
+
+template <typename Type>
+inline bool operator>(const SimpleVector<Type>& lhs, const SimpleVector<Type>& rhs) {
+    return rhs < lhs;
+}
+
+template <typename Type>
+inline bool operator>=(const SimpleVector<Type>& lhs, const SimpleVector<Type>& rhs) {
+    return !(lhs < rhs);
+}
